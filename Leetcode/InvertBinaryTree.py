6ec37e24adda293fbe2b35e1e338746feb62da3d
ValeriuNavitaniuc <37089262+ValeraNaviton@users.noreply.github.com>
2021-07-25 15:12:34 -0400

    Update InvertBinaryTree.py

--- Leetcode/InvertBinaryTree.py
+++ Leetcode/InvertBinaryTree.py
@@ -1,38 +1,21 @@
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-    def insert(self, val):
-
-        if val is None:
-            return
-
-        if self.val:
-            if val < self.val:
-                if self.left is None:
-                    self.left = TreeNode(val)
-                else:
-                    self.left.insert(val)
-            elif val > self.val:
-                if self.right is None:
-                    self.right = TreeNode(val)
-                else:
-                    self.right.insert(val)
-        else:
-            self.val = val
-
-
+        
+        
 class Solution:
-
     def invertTree(self, root):
         if root:
+            
+            temp = root.left
             root.left = self.invertTree(root.right)
-            root.right = self.invertTree(root.left)
-
+            root.right = self.invertTree(temp)
+            
         return root
-
+        
 
 # [4,2,7,1,3,6,9]
 array = TreeNode(4)
